402a33c751b27e19e642a12ce4fecc989693ae23
cxhcxhcxh <your-email@example.com>
2025-12-03 10:49:12 +0800

    Import favicon dynamically in React

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import faviconUrl from '../public/favicon.png'
+
+// Set favicon dynamically
+const link = document.querySelector("link[rel~='icon']")
+if (link) {
+  link.href = faviconUrl
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
